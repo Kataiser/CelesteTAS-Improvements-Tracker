9be060c07d0f0e3e4c22704f8d2229774ca6eff2
Kataiser <mecharon1.gm@gmail.com>
2022-05-13 15:32:26 -0500

    Finished add_mods command

--- dm.py
+++ dm.py
@@ -2,10 +2,12 @@ import logging
 import os
 import re
 import time
+import zipfile
 from typing import Optional
 
 import discord
 import requests
+import yaml
 
 import utils
 from utils import plural, projects
@@ -28,8 +30,11 @@ async def command_help(message: discord.Message):
     message_split = message.content.split()
 
     if len(message_split) > 1 and message_split[1] in command_functions:
-        command_doc = command_functions[message_split[1]].__doc__.replace('\n    ', '\n')
-        await message.channel.send(f"```\n{command_doc}```")
+        try:
+            command_doc = command_functions[message_split[1]].__doc__.replace('\n    ', '\n')
+            await message.channel.send(f"```\n{command_doc}```")
+        except AttributeError:
+            log.error(f"{command_functions[message_split[1]]} has no docstring")
     else:
         response = "Alright, looks you want to add your TAS project to this bot (or are just curious about what the help command says). Awesome! So, steps:" \
                    "\n\n1. Contact Kataiser#4640 that you're adding a new project. Theoretically this process doesn't need him, but realistically it's probably broken and/or janky, " \
@@ -137,7 +142,12 @@ async def command_register_project(message: discord.Message):
 
 
 async def command_add_mods(message: discord.Message):
-    """"""
+    """
+    add_mods IMPROVEMENTS_CHANNEL_ID MODS
+
+      IMPROVEMENTS_CHANNEL_ID: Turn on developer mode in Discord advanced setting, then right click the channel and click Copy ID
+      MODS: The mod(s) used by your project, separated by spaces (dependencies are automatically handled). Ex: EGCPACK, WinterCollab2021, conquerorpeak103
+    """
 
     log.info("Handling 'add_mods' command")
     message_split = message.content.split()
@@ -148,19 +158,21 @@ async def command_add_mods(message: discord.Message):
         return
 
     project_id = int(message_split.pop(1))
-    mods = [mod.removesuffix('.zip') for mod in message_split[1:]]
-    installed_mods = [item.removesuffix('.zip') for item in os.listdir(r'E:\Big downloads\celeste\Mods') if item.endswith('.zip')]
+    mods_given = [mod.removesuffix('.zip') for mod in message_split[1:]]
     project_mods = set(projects[project_id]['mods'])
     log.info(f"{len(project_mods)} mod(s) before adding: {project_mods}")
-    project_mods.update(mods)
+    project_mods = project_mods.union(mods_given)
+
+    for mod_given in mods_given:
+        project_mods = project_mods.union(get_mod_dependencies(mod_given))
+
     log.info(f"{len(project_mods)} mod(s) after adding: {project_mods}")
     projects[project_id]['mods'] = list(project_mods)
     utils.save_projects()
     mods_missing = set()
+    installed_mods = [item.removesuffix('.zip') for item in os.listdir(r'E:\Big downloads\celeste\Mods') if item.endswith('.zip')]
 
-    # TODO: auto handle dependencies
-
-    for mod in mods:
+    for mod in project_mods:
         if mod not in installed_mods:
             mods_missing.add(mod)
 
@@ -177,6 +189,23 @@ async def command_add_category(message: discord.Message):
     """"""
 
     await message.channel.send("Not yet implemented")
+
+
+# TODO: make recursive (if necessary)
+def get_mod_dependencies(mod: str) -> list:
+    zip_path = f'E:\\Big downloads\\celeste\\Mods\\{mod}.zip'
+
+    if not os.path.isfile(zip_path):
+        return []
+
+    with zipfile.ZipFile(zip_path) as mod_zip:
+        if zipfile.Path(mod_zip, 'everest.yaml').is_file():
+            with mod_zip.open('everest.yaml') as everest_yaml:
+                mod_everest = yaml.safe_load(everest_yaml)
+        else:
+            return []
+
+    return [d['Name'] for d in mod_everest[0]['Dependencies'] if d['Name'] != 'Everest']
 
 
 command_functions = {'help': command_help, 'register_project': command_register_project, 'add_category': command_add_category, 'add_mods': command_add_mods}
