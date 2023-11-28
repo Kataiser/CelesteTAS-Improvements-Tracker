859fc367bb99d34dfa1b0e07f5ba757f1984a8d3
Kataiser <mecharon1.gm@gmail.com>
2023-11-27 19:44:31 -0600

    e

--- run2.ps1
+++ run2.ps1
@@ -1,3 +1,3 @@
 $proc_info = New-CimInstance -CimClass (Get-CimClass -ClassName Win32_ProcessStartup) -Property @{CreateFlags=16777216} -ClientOnly
-$arguments = @{CommandLine="python bot.py"; ProcessStartupInformation=$proc_info}
+$arguments = @{CommandLine="run.bat"; ProcessStartupInformation=$proc_info}
 Invoke-CimMethod -ClassName Win32_Process -Name Create -Arguments $arguments
