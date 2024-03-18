53003c276dcbe1cd49fa8fb72d74a0fb036a83f9
Kataiser <mecharon1.gm@gmail.com>
2024-03-18 12:00:35 -0500

    Fixed hydro 1984 regex

--- bot.py
+++ bot.py
@@ -327,10 +327,10 @@ main.safe_mode = safe_mode
 projects_startup = Optional[dict]
 substrings_1984 = ('kataiser', 'kata', 'warm fish', 'jaded', 'psycabob', 'shadowdrop', 'cosmic brain')
 substrings_1984_music = ('lab', 'psychokinetic', 'pk ', 'superluminary')
-substrings_1984_hydro = ('shatter', 'shong', 'shattersong', 'hydro', 'ss+')
+substrings_1984_hydro = ('shatter', 'shong', 'shattersong', 'hydro')
 re_1984 = re.compile('|'.join(fr'\b{re.escape(sub)}\b' for sub in substrings_1984))
 re_1984_music = re.compile('|'.join(fr'\b{re.escape(sub)}\b' for sub in substrings_1984_music))
-re_1984_hydro = re.compile('|'.join(fr'\b{re.escape(sub)}\b' for sub in substrings_1984_hydro))
+re_1984_hydro = re.compile('|'.join(fr'\b{re.escape(sub)}\b' for sub in substrings_1984_hydro) + r'|\bss\+(?=\b|\W|$)')
 re_1984_cabob = re.compile(r'\bcabob\b')
 re_1984_vamp = re.compile(r'\bvamp\b')
 
