e358701d76ea866915faa1a640ba4e9e3036ec80
Kataiser <mecharon1.gm@gmail.com>
2025-11-09 22:29:02 -0600

    Updater logging kinda

--- updater.py
+++ updater.py
@@ -19,23 +19,30 @@ def main():
     time.sleep(1)
     target_process = psutil.Process(target_pid)
     target_process.kill()
+    print("Killed target")
     time.sleep(0.5)
     parent_process = psutil.Process(parent_pid)
     parent_process.kill()
-    print("Killed processes")
+    print("Killed parent")
     time.sleep(1)
 
     subprocess.run('git pull')
     time.sleep(1)
+    print("Pulled")
 
     bot_starts = ('run.bat', 'CelesteTAS Improvements Tracker.exe', 'python bot.py')
 
     for bot_start in bot_starts:
         if os.path.isfile(bot_start):
             subprocess.Popen(bot_start, creationflags=0x00000010)
+            print(f"Started {bot_start}")
 
     time.sleep(10)
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception as error:
+        print(repr(error))
+        input()
