8d26c09b31c66314f09403ef567a2e03d735fc70
Kataiser <mecharon1.gm@gmail.com>
2026-01-30 16:51:39 -0600

    Made run.sh loop

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-source .venv/bin/activate
-uv run bot.py
+cd "$(dirname "$0")"
+while true; do
+    source .venv/bin/activate
+    uv run bot.py
+    exit_code=$?
+    if [ $exit_code -ne 666 ]; then
+        break
+    fi
+    git pull
+done
+read -p "Press enter to continue..."
